19356470ccb7714df2593e52da5e23fa4f678413
John Haverkamp <john@haverkamp.us>
2020-12-12 14:32:31 -0600

    Cleaned up inconsistent mix of tab and space indenting.

--- Dying PC.js
+++ Dying PC.js
@@ -1,13 +1,13 @@
 (async () => {
 if (!token) {
-  ui.notifications.warn("You must select yourself.");
-  return
+ui.notifications.warn("You must select yourself.");
+return
 }
 
 if(!game.combats.active) return ui.notifications.error(`No active combat.`);
 
 let template =
-    `
+	`
 
 <form>
 	<p>Token: <span style="color: darkred">${token.name}</span></p>
@@ -20,23 +20,23 @@ let template =
 let mustRoll = false;
 let clearFX = false;
 new Dialog({
-    title: "Dying",
-    content: template,
-    buttons: {
-        yes: {
-            icon: "<i class='fas fa-check'></i>",
-            label: "Apply",
-            callback: () => mustRoll = true
-        },
-        no: {
-            icon: "<i class='fas fa-times'></i>",
-            label: "Cancel",
-        },        
-     },
-    default: "yes",
-    close: html => {
-        if (mustRoll) {
-            (async () => {
+	title: "Dying",
+	content: template,
+	buttons: {
+		yes: {
+			icon: "<i class='fas fa-check'></i>",
+			label: "Apply",
+			callback: () => mustRoll = true
+		},
+		no: {
+			icon: "<i class='fas fa-times'></i>",
+			label: "Cancel",
+		},        
+	},
+	default: "yes",
+	close: html => {
+		if (mustRoll) {
+			(async () => {
 				if (html.find("#removeWounded")[0].checked)
 				{
 					await PF2eConditionManager.removeConditionFromToken(token.actor.data.items.find((x) => x.name == "Wounded")._id, token);
@@ -45,7 +45,7 @@ new Dialog({
 				}
 				const recover = html.find("#recover")[0].checked;
 				var woundValue = token.actor.data.data.attributes.wounded.value;
-                var dyingValue = token.actor.data.data.attributes.dying.value;
+				var dyingValue = token.actor.data.data.attributes.dying.value;
 				if (recover && dyingValue == 0)
 				{
 					ui.notifications.warn("You are not dying.");
@@ -54,7 +54,7 @@ new Dialog({
 				let cValue = 1;
 				let critical = html.find("#critical")[0].checked;
 				let rollChecks = html.find("#rollChecks")[0].checked;
-                if (critical)
+				if (critical)
 					cValue++;
 				if (dyingValue == 0)
 					cValue = cValue + woundValue;
@@ -62,7 +62,7 @@ new Dialog({
 				if (dyingValue == 0)
 				{
 					woundValue++;
-                    await PF2eConditionManager.addConditionToToken(condition, token);
+					await PF2eConditionManager.addConditionToToken(condition, token);
 					await PF2eConditionManager.updateConditionValue(token.actor.data.items.find((x) => x.name == "Dying")._id, token, cValue)
 					await updateHUD("Dying");
 				}
@@ -88,7 +88,7 @@ new Dialog({
 				{
 					await PF2eConditionManager.updateConditionValue(token.actor.data.items.find((x) => x.name == "Wounded")._id, token, woundValue)
 				}
-                await updateHUD("Wounded");
+				await updateHUD("Wounded");
 				if (!(recover && cValue >= dyingValue))
 				{
 					await updateHUD("Unconscious");
@@ -97,15 +97,15 @@ new Dialog({
 				await updateHUD("Prone");
 				await updateHUD("Flat-Footed");
 
-                await PF2eStatusEffects._createChatMessage(token)
-                if (!recover)
+				await PF2eStatusEffects._createChatMessage(token)
+				if (!recover)
 					cValue += dyingValue;
 				else
 					cValue = Math.max(dyingValue - cValue,0);
 					
 				await addWD("dying",cValue);
-                await addWD("wounded", woundValue);
-                if (dyingValue == 0 && !recover)
+				await addWD("wounded", woundValue);
+				if (dyingValue == 0 && !recover)
 					await changeInit();
 				if (rollChecks)
 				{
@@ -129,44 +129,44 @@ new Dialog({
 					if (alert)
 						TurnAlert.delete(game.combat.data._id,alert.id);
 				}
-            })();
-        }
-        if (clearFX) {
-            deathFX("remove")
-        }
-    }
-    
+			})();
+		}
+		if (clearFX) {
+			deathFX("remove")
+		}
+	}
+	
 }, {
-    id: 'pf2-template-creator'
+	id: 'pf2-template-creator'
 }).render(true);
 
 function updateHUD(type) {
-    let updates = [];
-    let item = token.actor.items.find(i => i.name === type);
-    console.log("item: "+item)
-    updates.push({ "_id": item._id, "data.sources.hud": true });
-    actor.updateEmbeddedEntity("OwnedItem", updates);
+	let updates = [];
+	let item = token.actor.items.find(i => i.name === type);
+	console.log("item: "+item)
+	updates.push({ "_id": item._id, "data.sources.hud": true });
+	actor.updateEmbeddedEntity("OwnedItem", updates);
 }
 
 function addWD(type,cValue) {
-    console.log(cValue)
-    if(cValue >= 0 && cValue <= 4) {
+	console.log(cValue)
+	if(cValue >= 0 && cValue <= 4) {
 		if (type == "dying")
 			actor.update({"data.attributes.dying.value":cValue})
 		else
 			actor.update({"data.attributes.wounded.value":cValue})
-    }
+	}
 }
 
 function changeInit() {
-    //change initiative
-    let update = duplicate(game.combats.active);
-    let currentTokenID = game.combats.active.current.tokenId
-    let currentInit = game.combats.active.data.combatants.find(c => c.tokenId === currentTokenID).initiative
-    let combatant = update.combatants.find(i=>i.tokenId===token.id)
-    combatant.initiative = parseInt(currentInit)+0.6;
-    game.combats.active.update(update);
+	//change initiative
+	let update = duplicate(game.combats.active);
+	let currentTokenID = game.combats.active.current.tokenId
+	let currentInit = game.combats.active.data.combatants.find(c => c.tokenId === currentTokenID).initiative
+	let combatant = update.combatants.find(i=>i.tokenId===token.id)
+	combatant.initiative = parseInt(currentInit)+0.6;
+	game.combats.active.update(update);
 }
 
 
-            })();
+			})();
