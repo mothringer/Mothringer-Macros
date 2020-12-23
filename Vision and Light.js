5b8fb434d3b31c7877571618489653b708f9af4a
John Haverkamp <john@haverkamp.us>
2020-12-23 15:38:35 -0600

    Added prototype token update option to light macro

--- Vision and Light.js
+++ Vision and Light.js
@@ -19,12 +19,32 @@ let template =
 	<p><input type="radio" name="light" value="None"/> None
 	<input type="radio" name="light" value="Torch"/> Torch/Light Spell
 	<input type="radio" name="light" value="Light"/> Light Spell (Heightened)</p>
+	<p><input type="checkbox" id="updateActor" style="width: 80px margin: 0 5px 0 5px" value="true" />Update actor's prototype token also</p>
 </form>`;
 
 let doUpdate = false;
 
-function tokenUpdate(data){
-	canvas.tokens.controlled.map(token => token.update(data));
+
+function tokenUpdate(data,updateActorPrototype){
+	for (let currentToken of canvas.tokens.controlled)
+	{
+		currentToken.update(data);
+		if (updateActorPrototype)
+		{
+			let prototypeToken = game.actors.find(actor => actor._id === canvas.tokens.controlled[0].data.actorId).data.token;
+			if (data.vision)
+			{
+				prototypeToken.dimSight = data.dimSight;
+				prototypeToken.brightSight = data.brightSight;
+				prototypeToken.vision = data.vision;
+			}
+			if (data.dimLight)
+			{
+				prototypeToken.dimLight = data.dimLight;
+				prototypeToken.brightLight = data.brightLight;
+			}
+		}
+	}
 }
 
 new Dialog({
@@ -43,6 +63,7 @@ new Dialog({
 	},
 	default: "yes",
 	close: html => {
+		let updateActorPrototype = html.find("#updateActor")[0].checked;
 		for (const vision of document.getElementsByName("vision")) {
 			if (vision.checked) var visionType = vision.value;
 		}
@@ -75,7 +96,7 @@ new Dialog({
 				update.brightLight = 60;
 				break;
 		}
-		if (doUpdate) tokenUpdate(update);
+		if (doUpdate) tokenUpdate(update,updateActorPrototype);
 	}
 }, {
 	id: 'pf2-template-creator'
